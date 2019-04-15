26697bea4c09c8080edaec2b04f76f31f28cc20a
Kelvin Kni <keltheceo@gmail.com>
2019-04-15 15:58:18 -0700

    1st commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const logger = require('morgan')
+
+const app = express()
+
+// 3rd party 🎉 Logger middleware
+app.use(logger('tiny'))
+
+/**
+ * @desc :example Route-Handler for hits to this EndPoint
+ * It check the Request params for input & send info back
+ * in the form of Json
+ */
+app.get('/random/:min/:max/:word', (req, res) => {
+    const min = parseInt(req.params.min)
+    const max = parseInt(req.params.max)
+    const greeting = req.params.word
+
+    if (isNaN(min) || isNaN(max)) {
+        res.status(400)
+        res.json({ error: 'Bad request.' })
+        return
+    }
+
+    const result = Math.round(Math.random() * (max - min) + min)
+    // playing with
+    res.json({
+        result: result,
+        word: greeting,
+    })
+})
+
+/**
+ * @desc :example for api C.R.U.D operations
+ */
+app.get('/', (req, res) => {
+    res.end('you just send a GET request, friend')
+})
+
+app.post('/', (req, res) => {
+    res.send('a POST request? nice')
+})
+
+app.put('/', (req, res) => {
+    res.send("i don't see a lot of PUT request anymore")
+})
+
+app.delete('/', (req, res) => {
+    res.send('oh my, a DELETE?')
+})
+
+app.listen(3000, () => console.log('App is running @ port 3000'))
